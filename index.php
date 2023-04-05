f9289e30600cdd9e3079b53dfe57feb6dfa9d15f
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-05 14:51:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
         function create_user($login, $password)
         {
+            if (empty($login) && empty($password)) {
                 file_put_contents("/var/www/html/users/".$login, $password);
+            }
         }
         create_user("admin", "toto");
 ?>
