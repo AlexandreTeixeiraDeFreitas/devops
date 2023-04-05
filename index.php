38570a26aba51636bde5bd28986d9c9bd3c03419
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-05 15:28:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,125 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Créer un utilisateur</title>
+    <style>
+        body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+        }
+
+        h1 {
+        text-align: center;
+        margin-top: 50px;
+        }
+
+        form {
+        margin: 0 auto;
+        width: 400px;
+        background-color: #fff;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0px 0px 10px #ccc;
+        }
+
+        label {
+        display: block;
+        margin-bottom: 10px;
+        }
+
+        input[type="text"], input[type="password"] {
+        width: 100%;
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        margin-bottom: 20px;
+        }
+
+        input[type="submit"] {
+        background-color: #4CAF50;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+        background-color: #3e8e41;
+        }
+
+        .error {
+        color: red;
+        font-weight: bold;
+        margin-top: 10px;
+        }
+
+    </style>
+</head>
+<body>
+	<h1>Créer un utilisateur</h1>
+	<form method="post" action="">
+		<label for="login">Login :</label>
+		<input type="text" name="login" id="login" required><br><br>
+		<label for="password">Mot de passe :</label>
+		<input type="password" name="password" id="password" required><br><br>
+		<input type="submit" name="submit" value="Créer l'utilisateur">
+	</form>
+	<?php
+		if (isset($_POST['submit'])) {
+			$login = $_POST['login'];
+			$password = $_POST['password'];
+			create_user($login, $password);
+		}
         function create_user($login, $password)
         {
-            if (empty($login) && empty($password)) {
-                file_put_contents("/var/www/html/users/".$login, $password);
+            if (!empty($login) && !empty($password)) {
+                if (ctype_alpha($login) && preg_match('/^[a-zA-Z]+$/', $login)) {
+                    if (verifyPassword($password)) {
+                        file_put_contents("/var/www/html/users/".$login, $password);
+                        echo "Utilisateur créé avec succès !";
+                    }else{
+                        echo "Le mot de passe ne respecte pas les critères de sécurité.";
+                    }
+                }else{
+                    echo "Le login ne doit pas contenir de chiffre ou de caractère spécial.";
+                }
+            }else{
+                echo "Veuillez remplir tous les champs.";
             }
         }
-        create_user("admin", "toto");
-?>
+        function verifyPassword($password) {
+            // Vérification de la longueur
+            if (strlen($password) < 8) {
+                return false;
+            }
+            // Vérification de la présence d'une lettre majuscule
+            if (!preg_match('/[A-Z]/', $password)) {
+                return false;
+            }
+            // Vérification de la présence d'une lettre minuscule
+            if (!preg_match('/[a-z]/', $password)) {
+                return false;
+            }
+            // Vérification de la présence d'un chiffre
+            if (!preg_match('/[0-9]/', $password)) {
+                return false;
+            }
+            // Vérification de la présence d'un caractère spécial
+            if (!preg_match('/[\W]/', $password)) {
+                return false;
+            }
+            // Vérification de la présence de caractères spéciaux autorisés
+            if (preg_match('/[^a-zA-Z0-9!@#$%^&*()_+-=]/', $password)) {
+                return false;
+            }
+            // Vérification de la présence de caractères interdits
+            if (preg_match('/[<>]/', $password)) {
+                return false;
+            }
+            // Le mot de passe respecte tous les critères
+            return true;
+        }
+	?>
+</body>
+</html>
