58b80795e754e911101ddc8f8224c615bd2deffd
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-06 15:03:27 +0200

    test terminer

--- fonction.php
+++ fonction.php
@@ -6,28 +6,29 @@
 		}
         function create_user($login, $password)
         {
-
+            $filePath = __DIR__ . "/users/" . $login . ".txt";
+            $filePath = str_replace("\\", "/", $filePath);
             if (!empty($login) && !empty($password)) {
                 if (ctype_alpha($login) && preg_match('/^[a-zA-Z]+$/', $login)) {
                     if (verifyPassword($password)) {
-                        if (!file_exists(__DIR__ . "/users/" . $login . ".txt")) {
-                            file_put_contents(__DIR__ . "/users/" . $login . ".txt", $password);
-                            // printf("Utilisateur créé avec succès !");
+                        if (!file_exists($filePath)) {
+                            file_put_contents($filePath, $password);
+                            printf("Utilisateur créé avec succès !");
                             return 0;
                         } else {
-                            // printf("Un utilisateur avec ce login existe déjà.");
+                            printf("Un utilisateur avec ce login existe déjà.");
                             return 1;
                         }
                     }else{
-                        // printf("Le mot de passe ne respecte pas les critères de sécurité.");
+                        printf("Le mot de passe ne respecte pas les critères de sécurité.");
                         return 2;
                     }
                 }else{
-                    // printf("Le login ne doit pas contenir de caractère spécial.");
+                    printf("Le login ne doit pas contenir de caractère spécial.");
                     return 3;
                 }
             }else{
-                // printf("Veuillez remplir tous les champs.");
+                printf("Veuillez remplir tous les champs.");
                 return 4;
             }
         }
