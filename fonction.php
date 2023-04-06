13ebf840e902668e2d6fda3a4b91eca05f60812a
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-06 10:28:20 +0200

    Update fonction.php

--- fonction.php
+++ fonction.php
@@ -9,7 +9,14 @@
             if (!empty($login) && !empty($password)) {
                 if (ctype_alpha($login) && preg_match('/^[a-zA-Z]+$/', $login)) {
                     if (verifyPassword($password)) {
-                        file_put_contents("users/".$login, $password);
+                        if (!file_exists("users/".$login)) {
+                            file_put_contents("users/".$login, $password);
+                            printf("Utilisateur créé avec succès !");
+                            return true;
+                        } else {
+                            printf("Un utilisateur avec ce login existe déjà.");
+                            return false;
+                        }
                         printf("Utilisateur créé avec succès !");
                         return true;
                     }else{
