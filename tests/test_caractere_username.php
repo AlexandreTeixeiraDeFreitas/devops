bb56deaa76bbaa025f682c92697afc93714fdc5b
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-07 18:53:50 +0200

    tests

--- tests/test_caractere_username.php
+++ tests/test_caractere_username.php
@@ -6,8 +6,8 @@
     $r4 = create_user("user123", "P@ssw0rd!");
     $r5 = connect_user('username\' OR 1=1 --', 'p@ssword1');
     $r6 = connect_user('username1741852963741852963azertyuiopdfghjklvbncvghj', 'p@ssword1');
-    printf("r1: ".$r1." r2: ".$r2." r3: ".$r3." r4: ".$r4);
-    if ($r1 < 2 && $r2 == 3 && $r3 == 3 && $r4 = 3) {
+    // printf("r1: ".$r1." r2: ".$r2." r3: ".$r3." r4: ".$r4." r5: ".$r5." r6: ".$r6);
+    if ($r1 < 2 && $r2 == 3 && $r3 == 3 && $r4 = 3 && $r5 = 3 && $r6 = 3) {
         printf("true");
     } else {
         printf("false");
