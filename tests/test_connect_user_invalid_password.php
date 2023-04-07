e4414aca1191d2ecf18f758f115f13fc852c5454
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-07 11:30:59 +0200

    v

--- tests/test_connect_user_invalid_password.php
+++ tests/test_connect_user_invalid_password.php
@@ -6,7 +6,7 @@ $r1 = connect_user('username1', 'wrong_password');
 $r2 = connect_user('username1', 'P@sswor');
 $r3 = connect_user('username1', 'P@sswor');
 
-printf("r1: ".$r1." r2: ".$r2." r3: ".$r3);
+// printf("r1: ".$r1." r2: ".$r2." r3: ".$r3);
 
 if ($r1 === 1 && $r2 === 1 && $r3 === 1) {
     printf("true");
