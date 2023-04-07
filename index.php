9a0e92b2fe551eb8e0802f0dd4df7a49ed232141
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-07 13:45:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
     <p><a href="register.html">S'inscrire</a></p>
     <?php
     include 'fonction.php';
-    session_start();
+    // session_start();
     if (isset($_POST['submit'])) {
         $login = $_POST['login'];
         $password = $_POST['password'];
