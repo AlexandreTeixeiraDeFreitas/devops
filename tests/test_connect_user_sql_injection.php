c2e0e54dd6655675c1fa4e93955680fa853798ab
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-04-07 19:30:32 +0200

    Update test_connect_user_sql_injection.php

--- tests/test_connect_user_sql_injection.php
+++ tests/test_connect_user_sql_injection.php
@@ -1,14 +1,14 @@
 <?php
 include '../fonction.php';
 
-create_user("username", "p@ssword1");
+create_user("username8", "P@ssword1");
 session_start();
-$r1 = connect_user('username', 'p@ssword1');
-$r2 = connect_user('username', 'wrong_password');
+$r1 = connect_user('username8', 'P@ssword1');
+$r2 = connect_user('username8', 'wrong_password');
 $r3 = connect_user('non_existent_user', 'p@ssword1');
-$r4 = connect_user('username\' OR 1=1 --', 'p@ssword1');
-
-if ($r1 !== 0 && $r2 !== 0 && $r3 !== 0 && $r4 !== 0) {
+$r4 = connect_user('username8\' OR 1=1 --', 'p@ssword1');
+    // printf("r1: ".$r1." r2: ".$r2." r3: ".$r3." r4: ".$r4);
+if ($r1 === 0 && $r2 === 1 && $r3 === 3 && $r4 === 3) {
     printf("true");
 } else {
     printf("false");
